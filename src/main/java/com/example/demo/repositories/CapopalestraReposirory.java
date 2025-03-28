c81f4c873fbf3dc3ec04f10f273558e88f9bf733
Marco Ambrogio <maerlyn@hotmail.it>
2025-03-28 19:48:05 +0100

    Repositories created

---
+++ src/main/java/com/example/demo/repositories/CapopalestraReposirory.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.Capopalestra;
+
+public interface CapopalestraReposirory extends JpaRepository<Capopalestra, Long>{
+
+}
---
+++ src/main/java/com/example/demo/repositories/RegioniRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.Regioni;
+
+public interface RegioniRepository extends JpaRepository<Regioni, Long> {
+
+}
