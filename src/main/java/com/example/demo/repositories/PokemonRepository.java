00430ee26e542a11795565dd83541b3281d70934
Marco Ambrogio <maerlyn@hotmail.it>
2025-03-29 19:39:14 +0100

    aggiunta query resetID

--- src/main/java/com/example/demo/repositories/PokemonRepository.java
+++ src/main/java/com/example/demo/repositories/PokemonRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.example.demo.entities.Pokemon;
 
 public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
 
+	//query per resettare auto_increment
+	@Modifying
+	@Query(value = "ALTER TABLE pokemon AUTO_INCREMENT = 1", nativeQuery = true)	//query che resetta
+	void resetAutoIncrement();
 }
