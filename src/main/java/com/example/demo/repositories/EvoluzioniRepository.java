f1667980742b1daa924a6f788b9c7d674e89ee00
Marco Ambrogio <maerlyn@hotmail.it>
2025-03-29 19:41:05 +0100

    aggiunto un optional per evoluzioni

--- src/main/java/com/example/demo/repositories/EvoluzioniRepository.java
+++ src/main/java/com/example/demo/repositories/EvoluzioniRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.Evoluzioni;
 
 public interface EvoluzioniRepository extends JpaRepository<Evoluzioni, Long> {
+	Optional<Evoluzioni> findByNome(String nome);
 
 }
